1174a42ba8778bc86e15881ab9c26178a83787bd
Madssimo <gian0897@gmail.com>
2022-06-27 21:10:55 -0500

    project config

---
+++ skulltown/src/contracts/libraries/Skulltown.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.15;
+
+contract Skulltown {
+
+    string public name;
+    string public symbol;
+
+    constructor() {
+        name = 'Skulltown';
+        symbol = 'SKLLT';
+    }
+}
